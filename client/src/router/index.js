3f9afdc23e21feba1665de7a0594857268077128
yanzhiqiang <jonney0828@gmail.com>
2017-08-22 18:52:35 +0800

    add

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Dashboard from '@/components/Dashboard';
+import Register from '@/components/Register';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'Dashboard',
       component: Dashboard,
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register,
     },
   ],
 });
